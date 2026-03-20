216ab873a171b8068a451ba860464e90fe037c2d
Pc0stas <pablocostasnieto@gmail.com>
2026-03-20 16:51:43 +0100

    Update help text and snapcraft.yaml for improved service descriptions and versioning

--- help.go
+++ help.go
@@ -11,9 +11,15 @@ Usage: conga <service> <command>
 
 Available services:
   nginx     - Nginx configuration generator
+  docker    - Docker configuration generator
+  version   - Show CONGA version
+  help      - Show this help message
 
 Examples:
   conga nginx generate     - Generate Nginx config
   conga nginx help         - Show Nginx help
+  conga docker generate    - Generate Docker config
+  conga docker help        - Show Docker help
+  conga version            - Show CONGA version
   conga help               - Show this help`)
 }
